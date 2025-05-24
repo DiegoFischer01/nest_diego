2c224906e72c9732d3dac11f4ad8cfe1809120a7
Diego Fischer <diegof01gm@gmail.com>
2025-05-24 12:06:07 -0300

    cambios en clase de karen 23-5

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, /*Post */} from '@nestjs/common';
+import { AppService, iTrack } from './app.service';
 
-@Controller(/*api*/)
+ @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getTracks(): iTrack[] {
+    return this.appService.getTracks();
   }
+
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
 
+export interface iTrack {
+  id:number,
+  title:string,
+  duration:number,
+  artist: string
+}
+
+export const tracks: iTrack[] = [ //esto no es lo que corresponde
+{
+  id:1,
+  title: "cancion 1",
+  duration: 130,
+  artist: "interprete 1"
+},
+{
+  id:2,
+  title: "cancion 2",
+  duration: 140,
+  artist: "interprete 2"
+}
+
+]
+
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getTracks(): iTrack[] {
+    return tracks;
   }
 }
