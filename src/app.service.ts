e2b8bb37ae9b9ca6d67f4f4035ae9acfe70e0a37
Diego Fischer <diegof01gm@gmail.com>
2025-06-02 10:13:01 -0300

    actualice los codigo con lo que paso una compañera

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,5 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getTracks():string{
-    return "hola"
-  }
+  
 }
--- src/track/track.interface.ts
+++ src/track/track.interface.ts
@@ -1,1 +1,4 @@
-export interface Track {}
+export interface Track {
+
+    
+}
