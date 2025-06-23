ebc7588de3667b28add8034edcb22138497ae4e5
Diego Fischer <diegof01gm@gmail.com>
2025-06-23 20:12:15 -0300

    clase 23.06.25

--- src/track/track.controller.ts
+++ src/track/track.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Param, Post, Body, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Delete, Put, HttpCode, HttpStatus, Res } from '@nestjs/common';
 import { TrackService } from './track.service';
 import { Track } from './track.interface';
+import { error } from 'console';
 
 @Controller('track')
 export class TrackController {
-  constructor(private readonly trackService: TrackService) {}
+  constructor(private readonly trackService: TrackService) { }
 
   @Get()
   getTracks(): Promise<Track[]> {
@@ -12,23 +13,24 @@ export class TrackController {
   }
 
   @Get(':id')
-  getTrackById(@Param('id') id: number): Promise<Track> {
+  getTrackById(@Param('id') id: number): Promise<Track | undefined> {
     return this.trackService.getTrackById(id);
   }
 
   @Post()
-  createTrack(@Body() body:Track): Promise<Track> {
+  createTrack(@Body() body: Track): Promise<Track> {
     return this.trackService.createTrack(body);
-    }
+  }
 
   @Delete(':id')
-  deleteTrackById(@Param('id') id:number) : Promise<Track> {
+  deleteTrackById(@Param('id') id: number): Promise<Track> {
     return this.trackService.deleteTrackById(id);
   }
 
   @Put(':id')
+  @HttpCode(204)
   updateTrackById(@Param('id') id: number, @Body() body: Track) {
     return this.trackService.updateTrackById(id, body);
   }
-    
+
 }
