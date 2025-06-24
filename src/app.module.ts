8d72e47155d5fd068af097b493d59285ff217738
Diego Fischer <diegof01gm@gmail.com>
2025-06-23 21:00:03 -0300

    use: nest generate module track

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { TrackController } from './track/track.controller';
 import { TrackService } from './track/track.service';
+import { TrackModule } from './track/track.module';
 
 
 @Module({
-  imports: [ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'client') })],
+  imports: [ServeStaticModule.forRoot({ rootPath: join(__dirname, '..', 'client') }), TrackModule],
   controllers: [AppController, TrackController],
   providers: [AppService, TrackService],
 })
